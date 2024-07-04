b866af44c26c8dd59d9b2cb701a3fcd3ac452aa9
hamzakamran <hamzakwebdev@gmail.com>
2024-07-04 14:35:53 -0500

    Meta

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./features/store/store.js";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<BrowserRouter>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</BrowserRouter>
+	<HelmetProvider>
+		<BrowserRouter>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</BrowserRouter>
+	</HelmetProvider>
 );
